c95ce925fcc1479107da1ce4a2b064a73b57bfe6
YELEMCOURE OUAHABOU <ouahabouyelemcoure@gmail.com>
2026-04-27 06:51:06 +0000

    fix: conditional stripe init for build

--- my-app/lib/stripe.ts
+++ my-app/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_API_KEY ?? "placeholder", {
   apiVersion: "2026-04-22.dahlia",
   typescript: true,
 });
