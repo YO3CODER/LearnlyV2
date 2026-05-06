db77d02836543bdb2f91e597278d6b2074377b96
YELEMCOURE OUAHABOU <ouahabouyelemcoure@gmail.com>
2026-05-06 18:47:24 +0000

    mise à jour

--- my-app/app/admin/challenge/edit.tsx
+++ my-app/app/admin/challenge/edit.tsx
@@ -1,37 +1,24 @@
 import { SimpleForm, Edit, TextInput, ReferenceInput, NumberInput, required, SelectInput } from "react-admin";
 
+const CHALLENGE_TYPES = [
+  { id: "SELECT", name: "SELECT" },
+  { id: "ASSIST", name: "ASSIST" },
+  { id: "WORD_BANK", name: "WORD_BANK" },
+  { id: "FILL_BLANK", name: "FILL_BLANK" },
+  { id: "TRANSLATE", name: "TRANSLATE" },
+  { id: "MATCH", name: "MATCH" },
+  { id: "LISTEN", name: "LISTEN" },
+];
+
 export const ChallengeEdit = () => {
   return (
     <Edit>
       <SimpleForm>
-        <TextInput 
-          source="question" 
-          validate={[required()]} 
-          label="Question"
-        />
-        <SelectInput
-          source="type"
-          choices={[
-            {
-              id: "SELECT",
-              name: "SELECT",
-            },
-            {
-              id: "ASSIST",
-              name: "ASSIST",
-            }
-          ]}
-          validate={[required()]} 
-        />
-        <ReferenceInput
-          source="lessonId"
-          reference="lessons"
-        />
-        <NumberInput
-          source="order"
-          validate={[required()]}
-          label="Order"
-        />
+        <TextInput source="id" disabled label="ID" />
+        <TextInput source="question" validate={[required()]} label="Question" fullWidth />
+        <SelectInput source="type" choices={CHALLENGE_TYPES} validate={[required()]} />
+        <ReferenceInput source="lessonId" reference="lessons" />
+        <NumberInput source="order" validate={[required()]} label="Order" />
       </SimpleForm>
     </Edit>
   );
--- my-app/app/admin/challenge/list.tsx
+++ my-app/app/admin/challenge/list.tsx
@@ -1,26 +1,24 @@
 import { Datagrid, List, TextField, ReferenceField, NumberField, SelectField } from "react-admin";
 
+const CHALLENGE_TYPES = [
+  { id: "SELECT", name: "SELECT" },
+  { id: "ASSIST", name: "ASSIST" },
+  { id: "WORD_BANK", name: "WORD_BANK" },
+  { id: "FILL_BLANK", name: "FILL_BLANK" },
+  { id: "TRANSLATE", name: "TRANSLATE" },
+  { id: "MATCH", name: "MATCH" },
+  { id: "LISTEN", name: "LISTEN" },
+];
+
 export const ChallengeList = () => {
   return (
-    <List>
+    <List sort={{ field: "id", order: "ASC" }} perPage={25}>
       <Datagrid rowClick="edit">
-        <TextField source="id" />
-        <TextField source="question" />
-        <SelectField
-          source="type"
-          choices={[
-            {
-              id: "SELECT",
-              name: "SELECT",
-            },
-            {
-              id: "ASSIST",
-              name: "ASSIST",
-            }
-          ]}
-        />
-        <ReferenceField source="lessonId" reference="lessons" />
-        <NumberField source="order" />
+        <NumberField source="id" label="ID" />
+        <TextField source="question" label="Question" />
+        <SelectField source="type" choices={CHALLENGE_TYPES} label="Type" />
+        <ReferenceField source="lessonId" reference="lessons" label="Leçon" />
+        <NumberField source="order" label="Order" />
       </Datagrid>
     </List>
   );
